58d186e437014c5a1eb370594693fa13bbc1793b
Cathal O'Connor <catoconn@redhat.com>
2021-04-15 11:42:14 +0100

    Made an update

--- src/main/java/com/redhat/fuse/boosters/rest/http/Application.java
+++ src/main/java/com/redhat/fuse/boosters/rest/http/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     /**
-     * Main method to start the application.
+     * Main method to start the application 3cale.
      */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
